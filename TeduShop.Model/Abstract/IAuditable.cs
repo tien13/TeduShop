d62ce132fa83d46efa128eb4095347435826de06
MinhTien <ngominhtien1@gmail.com>
2019-10-28 21:14:38 +0700

    Commit TeduShop.Model
    Class All Table

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    interface IAuditable
+    {
+        DateTime? CreateDate { set; get; }
+       
+        string CreatedBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+      
+        string UpdateBy { set; get; }
+        bool Status { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+    }
+}
---
+++ TeduShop.Model/Model/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Model
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        [Required]
+        public string Url { set; get; }
+
+
+        public int? DisplayOrder { set; get; }
+
+        [Required]
+     
+        public int GroupID { set; get; }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup Menugroup { set; get; }
+
+        public string Target { set; get; }
+
+        [Required]
+        public bool Status { set; get; }
+    }
+}
